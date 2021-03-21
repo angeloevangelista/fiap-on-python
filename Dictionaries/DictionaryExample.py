88b61456a2ffbb323cf060ffb641d311931bbe45
Angelo Evangelista <angeloevan.ane@gmail.com>
2021-03-20 23:56:27 -0300

    dictionaries

---
+++ Dictionaries/DictionaryExample.py
@@ -0,0 +1,23 @@
+users = {}
+
+users = {
+  'chaves': [
+    'Chaves do oito',
+    '24/12/2020',
+    'Recep_01',
+  ],
+  'quico': [
+    'Quico das Flores',
+    '20/12/2020',
+    'Raiox_03',
+  ],
+}
+
+users['florinda'] = [
+  'Florinda Flores',
+  '24/12/2020',
+  'Raiox_01',
+]
+
+print(users, '\n')
+print(users.get('chaves'))
