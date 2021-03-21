d6753144d8fda88da94d2052f8a228b3000bf18d
Angelo Evangelista <angeloevan.ane@gmail.com>
2021-03-21 19:09:52 -0300

    files: read

--- Files/MyFirstFile.py
+++ Files/MyFirstFile.py
@@ -1,2 +1,9 @@
-with open('temp_file.py', 'a') as file:
-  file.writelines('print(\' Hello, World!\')')
+with open('temp_file.py', 'r+') as file:
+  lines = file.readlines()
+
+  if len(lines) == 0:
+    file.writelines('print(\' Hello, World!\')\n')
+    print('File with no content, \'hello world\' added')
+  else:
+    for line in lines:
+      print(line)
